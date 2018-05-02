a3a85fdfa46a97a6324492b50773bedbb8fe8fb4
giora-omer <giora.omer@imperva.com>
2018-05-02 09:12:09 +0300

    removing sdk code for now

--- imperva_sdk/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2018 Imperva. All rights reserved.
- 
-from imperva_sdk.core import *
-from imperva_sdk.MxConnection import MxConnection
-
-__version__ = '0.1.7'
-__author__ = "Imperva Inc."
-
